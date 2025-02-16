a604ff9c566a3323911242ecdc3c6eca8d6c279b
RuslanV93 <ruslanvak93@gmail.com>
2025-02-16 09:12:55 +0300

    exception filter

--- src/core/exceptions/filters/all-exception.filter.ts
+++ src/core/exceptions/filters/all-exception.filter.ts
@@ -1,19 +1,27 @@
 import { Catch, HttpException, HttpStatus } from '@nestjs/common';
 import { BaseExceptionFilter, ErrorMessage } from './base-exception.filter';
-import { Response, Request } from 'express';
+import { Response } from 'express';
 
 // Интерфейс для описания структуры ошибки
-interface ResponseBody {
-  message: ErrorMessage[];
-  error: string;
-  statusCode: HttpStatus;
-}
 
 @Catch()
 export class AllExceptionsFilter extends BaseExceptionFilter {
-  onCatch(
-    exception: HttpException,
-    response: Response,
-    request: Request,
-  ): void {}
+  onCatch(exception: unknown, response: Response): void {
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    if (status === 400) {
+      const responseMessage: ErrorMessage = (exception as any).response.message;
+      response.status(status).json({
+        errorsMessages: responseMessage,
+      });
+    } else {
+      response
+        .status(status)
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        .json({ errorsMessages: (exception as any).response });
+    }
+  }
 }
